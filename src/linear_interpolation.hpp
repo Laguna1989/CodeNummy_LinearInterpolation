2de801a6fb599253fe4aaa6fd08ddb0045880cf4
Simon Weis <laguna.ws@gmail.com>
2021-08-06 16:07:34 +0200

    Add solution to branch

--- src/linear_interpolation.hpp
+++ src/linear_interpolation.hpp
@@ -6,6 +6,22 @@
 template <typename Tx, typename Ty>
 Ty linear_interpolation(Tx const x1, Tx const x2, Ty const y1, Ty const y2, Tx const x)
 {
+    if (x1 > x2)
+    {
+        throw std::invalid_argument{"x values are not ordered"};
+    }
+    if (x < x1 || x > x2) {
+        throw std::invalid_argument { "x range outside range." };
+    }
+    if (x == x1) {
+        return y1;
+    }
+    if (x == x2) {
+        return y2;
+    }
+
+    return y1 + (y2 - y1)/(x2 - x1) *(x- x1);
+
     // fake impl
     return 42.0;
 }
